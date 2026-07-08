f287df8c13df9c80c35251b79c173b8aa562be0a
Verdi <152645717+tfVerdi@users.noreply.github.com>
2026-07-08 14:57:24 -0400

    First instances of Ingredients and IngredientCategory
    Plus the fixes it took to make them lol

--- logic/models.py
+++ logic/models.py
@@ -1,31 +1,27 @@
 from django.db import models
 
-class Ingredient(models.Model):
-    def __init__(self):
-        name = models.CharField(max_length=64)
-        category = models.ForeignKey(IngredientCategory)
+class IngredientCategory(models.Model):
+    name = models.CharField(max_length=64)
     def __str__(self):
         return self.name
+    
+class MealCategory(models.Model):
+    name = models.CharField(max_length=64)
+    def __str__(self):
+        return self.name
+    
+class Ingredient(models.Model):
+    name = models.CharField(max_length=64)
+    category = models.ForeignKey(IngredientCategory, on_delete=models.CASCADE, null=True)
+    def __str__(self):
+        return f'{IngredientCategory.objects.get(pk=self.category.pk).name} - {self.name}'
 
 class Meal(models.Model):
-    def __init__(self):
-        name = models.CharField(max_length=64)
-        description = models.CharField(max_length=256)
-        instructions = models.CharField(max_length=2048)
-        category = models.ForeignKey(MealCategory)
-        ingredients = models.ManyToManyField(Ingredient)
-        optional_ingredients = models.ManyToManyField(Ingredient)
-    def __str__(self):
-        return self.name
-
-class IngredientCategory(models.Model):
-    def __init__(self):
-        name = models.CharField(max_length=64)
-    def __str__(self):
-        return self.name
-
-class MealCategory(models.Model):
-    def __init__(self):
-        name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256, null=True)
+    instructions = models.CharField(max_length=2048, null=True)
+    category = models.ForeignKey(MealCategory, on_delete=models.CASCADE, null=True)
+    ingredients = models.ManyToManyField(Ingredient, related_name="meal_ingredients")
+    optional_ingredients = models.ManyToManyField(Ingredient)
     def __str__(self):
         return self.name
