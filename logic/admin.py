bdbff0f05748a05f695ce3d53e692fed32e5c8f1
Verdi <152645717+tfVerdi@users.noreply.github.com>
2026-07-09 01:06:59 -0400

    Modified models and model views

--- logic/admin.py
+++ logic/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from django.db import models
 from logic.models import Ingredient, IngredientCategory, Meal, MealCategory
 
-admin.site.register(Ingredient, on_delete=models.CASCADE)
-admin.site.register(IngredientCategory, on_delete=models.CASCADE)
-admin.site.register(Meal, on_delete=models.CASCADE)
-admin.site.register(MealCategory, on_delete=models.CASCADE)
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+
+class IngredientCategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        IngredientInline,
+    ]
+
+admin.site.register(Ingredient, on_delete=models.DO_NOTHING)
+admin.site.register(IngredientCategory, IngredientCategoryAdmin, on_delete=models.DO_NOTHING)
+admin.site.register(Meal, on_delete=models.DO_NOTHING)
+admin.site.register(MealCategory, on_delete=models.DO_NOTHING)
--- logic/models.py
+++ logic/models.py
@@ -11,17 +11,17 @@ class MealCategory(models.Model):
         return self.name
     
 class Ingredient(models.Model):
-    name = models.CharField(max_length=64)
-    category = models.ForeignKey(IngredientCategory, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=64, unique=True)
+    category = models.ForeignKey(IngredientCategory, on_delete=models.DO_NOTHING, null=True)
     def __str__(self):
         return f'{IngredientCategory.objects.get(pk=self.category.pk).name} - {self.name}'
 
 class Meal(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
     description = models.CharField(max_length=256, null=True)
     instructions = models.CharField(max_length=2048, null=True)
-    category = models.ForeignKey(MealCategory, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(MealCategory, on_delete=models.DO_NOTHING, null=True)
     ingredients = models.ManyToManyField(Ingredient, related_name="meal_ingredients")
-    optional_ingredients = models.ManyToManyField(Ingredient)
+    optional_ingredients = models.ManyToManyField(Ingredient, blank=True)
     def __str__(self):
         return self.name
