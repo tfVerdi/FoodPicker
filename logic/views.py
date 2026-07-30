0fdef4e55138e30975659d90e895211f43b231c9
Verdi <152645717+tfVerdi@users.noreply.github.com>
2026-07-30 00:09:26 -0400

    Started dashboard

--- logic/views.py
+++ logic/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Meal, Ingredient
 
 # Create your views here.
+def dashboard(response):
+    return render(
+        response,
+        "dashboard/index.html",
+        {"ingredients": Ingredient.objects.all}
+    )
